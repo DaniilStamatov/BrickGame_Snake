7296f5aa96d63798d28bcc9a444cda51f1a6ad7e
DaniilStamatov <danstam63@gmail.com>
2025-02-09 11:24:12 +0300

    game field drawing okay

---
+++ src/build/CMakeFiles/MyExecutable_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable_autogen"
+  "MyExecutable_autogen/mocs_compilation.cpp"
+  "MyExecutable_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MyExecutable_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
